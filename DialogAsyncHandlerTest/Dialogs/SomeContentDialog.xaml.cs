55a73887908587e7d51983675bb40912fb5411fd
Hans Kesting <hans_kesting@hotmail.com>
2020-06-17 14:06:07 +0200

    Initial commit

---
+++ DialogAsyncHandlerTest/Dialogs/SomeContentDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace DialogAsyncHandlerTest.Dialogs
+{
+    public sealed partial class SomeContentDialog : ContentDialog
+    {
+        public SomeContentDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Result { get; set; } = "Initial";
+
+        /// <summary>
+        /// Try and use an async handler, but failing. The dialog exits on the first real await (but the code keeps running).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private async void ContentDialog_ButtonClick_Wrong(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            this.Result = "Still Getting";
+            var res = await GetSomethingAsync();
+            this.Result = res;
+        }
+
+        /// <summary>
+        /// The correct way of using an async handler here: a deferral.
+        /// </summary>
+        /// <remarks>
+        /// Because the handler needs a void return type, we need another way to signal completion. 
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private async void ContentDialog_ButtonClick_Correct(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            var defer = args.GetDeferral();
+
+            this.Result = "Still Getting";
+            var res = await GetSomethingAsync();
+            this.Result = res;
+
+            // finally mark the action as Complete
+            defer.Complete();
+        }
+
+        private async Task<string> GetSomethingAsync()
+        {
+            // simulate some long-running async work
+            await Task.Delay(1000);
+            return "Got it";
+        }
+    }
+}
---
+++ DialogAsyncHandlerTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DialogAsyncHandlerTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Dialogs.SomeContentDialog();
+            this.Result.Text = dlg.Result;
+            var res = await dlg.ShowAsync();
+            this.Result.Text = $"Button clicked: {res}; Result: {dlg.Result}";
+
+            await Task.Delay(1100);
+            this.Result.Text += $"; Result2: {dlg.Result}";
+        }
+    }
+}
